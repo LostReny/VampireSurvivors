86f7da4df7353270d441716cdf59054e3f5f4f3c
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-04-13 23:20:04 -0300

    Prototipo
    Finalizando prototipo

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    public GameObject explosion;
     public Rigidbody2D arrowRb;
     public float arrowSp;
 
@@ -18,6 +19,22 @@ public class Arrow : MonoBehaviour
     {
         Vector2 arrowDir = transform.position + transform.right * arrowSp * Time.deltaTime;
         arrowRb.MovePosition(arrowDir);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            //destroi explosion criada
+            GameObject exp = Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(exp, 1f);
+
+            //destroi inimigo -- tag
+            Destroy(collision.gameObject);
+
+            // destroi flecha
+            Destroy(gameObject);
+        }
     }
 
 }
